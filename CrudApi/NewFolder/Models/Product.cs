ad3c354769987e8cecf5155de35fa93354b566c1
saicharan-306 <saicharan.manpuri@gmail.com>
2025-06-25 15:33:57 -0500

    Meaningful message about the changes

--- CrudApi/NewFolder/Models/Product.cs
+++ CrudApi/NewFolder/Models/Product.cs
@@ -7,12 +7,15 @@
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
+
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Category is required")]
+
         public string Category { get; set; }
 
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+
         public decimal Price { get; set; }
     }
 
